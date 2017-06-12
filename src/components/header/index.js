9c52b91a7d9c89ca585c27291c19df5e017b0563
Mikhail Miropolskiy <the-ms@ya.ru>
2017-06-12 09:31:43 +0700

    banner

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,20 +3,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './styles.css';
+
 export default () => {
     return (
         <header>
-            <header>
-                <nav className="top-nav">
-                    <div className="container">
-                        <div className="nav-wrapper">
-                            <Link to={'/'} className="page-title">
-                                Интернет-магазины Новосибирска
-                            </Link>
-                        </div>
+            <nav className="top-nav">
+                <div className="container">
+                    <div className="nav-wrapper">
+                        <Link to={'/'} className="page-title">
+                            Интернет-магазины Новосибирска
+                        </Link>
                     </div>
-                </nav>
-            </header>
+                </div>
+            </nav>
         </header>
     );
 };
